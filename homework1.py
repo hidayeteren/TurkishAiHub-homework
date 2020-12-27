9075dacf3019d736c68e7477faca2fb1fe2d3736
hidayeteren <63645953+hidayeteren@users.noreply.github.com>
2020-12-27 18:17:23 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+variables1 = input("Birinci değeri giriniz : ")
+variables2 = input("İkinci değeri giriniz : ")
+variables3 = input("Üçüncü değeri giriniz : ")
+variables4 = input("Dördüncü değeri giriniz : ")
+variables5 = input("Beşinci değeri giriniz : ")
+
+print ('Girilen 5 değerimiz : {} {} {} {} {}'.format(variables1, variables2, variables3, variables4, variables5)) 
+print (type(variables1))
